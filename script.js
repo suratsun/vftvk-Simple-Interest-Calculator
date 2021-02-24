9ec8fc04a3f015e08f699479b75d4c474056cb06
Surat Suntong <suratsuntong@gmail.com>
2021-02-24 13:08:25 +0700

    Init Program
    create function to calculate interest and form validation.

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    year = document.getElementById("years").value;
+    interest = principal * year * rate / 100;
+
+    if(principal > 0) {
+        in_year = new Date().getFullYear() + parseInt(year)
+    
+        document.getElementById("result").innerHTML = "<p>If you deposit " + principal 
+            + " ,</p><p>at an interest rate of " + rate 
+            + "%.</p><p>You will receive an amount of "+ interest
+            + "</p><p>In the year " + in_year + "</p>"
+    }
+    else {
+        window.alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
     
 }
-        
+
+function change_rate() {
+    intallment_rate = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML = intallment_rate + '%';
+}
